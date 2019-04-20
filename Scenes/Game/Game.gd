105165a4cffdc6da7ba0977bb1c28b51154fe8ac
Kevin Thompson <kevin@kevinthompson.info>
2019-04-20 12:41:33 -0700

    add camera boundaries

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -2,32 +2,43 @@ extends Node2D
 
 # Editor Variables
 # ==============================
-export var alien_count : int = 5
-export var alien_scene : PackedScene
+export var tribe_count : int = 5
+export var tribe_scene : PackedScene
 
 # Internal Variables
 # ==============================
-var aliens = []
+var tribes = []
+onready var screen_width = get_viewport().size.x
+onready var screen_height = get_viewport().size.y
+var min_pos
+var max_pos
 
 # Lifecycle Methods
 # ==============================
 func _ready() -> void:
-  spawn_aliens()
+  var ctrans = get_canvas_transform()
+  var view_size = get_viewport_rect().size / ctrans.get_scale()
+
+  min_pos = -ctrans.get_origin() / ctrans.get_scale()
+  max_pos = min_pos + view_size
+
+  spawn_tribes()
 
 func _update() -> void:
   pass
 
 # Helper Methods
 # ==============================
+func spawn_tribes() -> void:
+  for i in range(tribe_count):
+    var spawn_position = Vector2(rand_range(min_pos.x + 64, max_pos.x - 64), rand_range(min_pos.y + 64, max_pos.y - 64))
+    var new_tribe = tribe_scene.instance()
+    new_tribe.position = spawn_position
+    tribes.append(new_tribe)
+    $World/Tribes.add_child(new_tribe)
 
-func spawn_aliens() -> void:
-  for i in range(alien_count):
-    var spawn_position = Vector2(rand_range(64,960), rand_range(64,536))
-    var new_alien = alien_scene.instance()
-    new_alien.position = spawn_position
-    new_alien.target = spawn_position
-    aliens.append(new_alien)
-    $World/Aliens.add_child(new_alien)
+# Note: rotation is not taken into account here. An improvement would be
+# to use the inverse transform instead of this.
 
 # Signal Events
 # ==============================
--- Scenes/Tribe/Tribe.gd
+++ Scenes/Tribe/Tribe.gd
@@ -8,19 +8,14 @@ export var alien_scene : PackedScene
 # Internal Variables
 # ==============================
 var aliens = []
-onready var screen_width = get_viewport().size.x
-onready var screen_height = get_viewport().size.y
 var min_pos
 var max_pos
 
 # Lifecycle Methods
 # ==============================
 func _ready() -> void:
-  var ctrans = get_canvas_transform()
-  var view_size = get_viewport_rect().size / ctrans.get_scale()
-
-  min_pos = -ctrans.get_origin() / ctrans.get_scale()
-  max_pos = min_pos + view_size
+  min_pos = Vector2(-40,-40)
+  max_pos = Vector2(40,40)
 
   spawn_aliens()
 
@@ -31,11 +26,11 @@ func _update() -> void:
 # ==============================
 func spawn_aliens() -> void:
   for i in range(alien_count):
-    var spawn_position = Vector2(rand_range(min_pos.x + 64, max_pos.x - 64), rand_range(min_pos.y + 64, max_pos.y - 64))
+    var spawn_position = Vector2(rand_range(min_pos.x, max_pos.x), rand_range(min_pos.y, max_pos.y))
     var new_alien = alien_scene.instance()
-    new_alien.position = spawn_position
+    new_alien.position = Vector2()
     aliens.append(new_alien)
-    $World/Aliens.add_child(new_alien)
+    add_child(new_alien)
 
 # Note: rotation is not taken into account here. An improvement would be
 # to use the inverse transform instead of this.
