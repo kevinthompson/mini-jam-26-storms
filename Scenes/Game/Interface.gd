40b7053cd94a9ff4e374e4077b513e8759d1c497
Kevin Thompson <kevin@kevinthompson.info>
2019-04-20 12:14:20 -0700

    Add storm buttons to interface

--- Scenes/Game/Interface.gd
+++ Scenes/Game/Interface.gd
@@ -1,7 +1,8 @@
-extends Control
+extends Node
 
 func _input(event: InputEvent) -> void:
   if event.is_action_pressed("pause"):
     var pause_state = !get_tree().paused
     get_tree().paused = pause_state
-    visible = pause_state
+    $Pause.visible = pause_state
+    $Controls.visible = !pause_state
